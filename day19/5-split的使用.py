df7823a27558354d996968a239e338dab2b2d3fc
hy <xx17866590431q1786659043@qq.com>
2022-07-01 00:11:16 +0800

    基础以及高阶部分

--- day19/5-split的使用.py
+++
@@ -1,6 +0,0 @@
-# 作者: 王道 龙哥
-# 2022年06月21日11时25分17秒
-import re
-
-ret = re.split(r":| ","info:xiaoZhang 33 shandong")
-print(ret)
--- day19/test.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-# auther@hy
-# 2022年06月30日
-print("上传到github")
-print("测试成功!")
